c5cc70356bf65897541632071200c44b2c94350e
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2022-02-14 14:33:27 -0300

    Calculando o tempo e finalizando o projeto

--- Program.cs
+++ Program.cs
@@ -12,8 +12,30 @@ static void Menu()
 
     string data = Console.ReadLine().ToLower();
     char type = char.Parse(data.Substring(data.Length - 1, 1));
-    Console.WriteLine(data);
-    Console.WriteLine(type);
+    int time = int.Parse(data.Substring(0, data.Length - 1));
+    int multiplier = 1;
+
+    if (type == 'm')
+        multiplier = 60;
+
+
+    if (time == 0)
+        System.Environment.Exit(0);
+
+    PreStart(time * multiplier);
+}
+
+static void PreStart(int time)
+{
+    Console.Clear();
+    Console.WriteLine("Ready...");
+    Thread.Sleep(1000);
+    Console.WriteLine("Set...");
+    Thread.Sleep(1000);
+    Console.WriteLine("Go...");
+    Thread.Sleep(2500);
+
+    Start(time);
 }
 
 static void Start(int time)
@@ -31,4 +53,5 @@ static void Start(int time)
     Console.Clear();
     Console.WriteLine("Stopwatch finalizado...");
     Thread.Sleep(2500);
+    Menu();
 }
